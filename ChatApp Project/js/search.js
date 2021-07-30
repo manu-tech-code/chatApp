54ba567afffa2bdec2116b33871990f346ba4452
ManuelDCoder <ekwabena57@gmail.com>
2021-07-30 22:27:16 +0000

    Update On ChatApp

--- ChatApp Project/js/search.js
+++
@@ -1,8 +0,0 @@
-const searchBar = document.querySelector(".user .search input");
-const searchbtn = document.querySelector(".user .search button");
-
-searchbtn.onclick = ()=> {
-    searchBar.classList.toggle("active");
-    searchBar.focus();
-    searchbtn.classList.toggle("active")
-}
---
+++ ChatApp Project/php/logout.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+    if(isset($_SESSION['unique_id'])){
+        include_once "config.php";
+        $logout_id = mysqli_real_escape_string($conn, $_GET['user_id']);
+        if(isset($logout_id)){
+            $status = "Offline now";
+            $sql = mysqli_query($conn, "UPDATE users SET status = '{$status}' WHERE unique_id={$logout_id}");
+            if($sql){
+                session_unset();
+                session_destroy();
+                header("location: ../login.php");
+            }
+        }else{
+            echo "Can't Log out";
+
+            header("location: ../users.php");
+        }
+    }else{  
+        echo "Error Logging out";
+        header("location: ../login.php");
+    }
+?>
